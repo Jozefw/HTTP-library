acbed48c91725e90cd6fb63c3f938292a22e5da3
Jozef <jozefw@sbcglobal.net>
2018-01-16 14:38:59 -0800

    add get to ajax linrary

--- easyhttp.js
+++ easyhttp.js
@@ -1,36 +1,21 @@
+function easyHTTP(){
+  this.http = new XMLHttpRequest();
+}
 
+// get request
 
+easyHTTP.prototype.get = function(url, cb) {
+  this.http.open('GET',url,true);
+ let self = this;
+  this.http.onload = function(){
+    if(self.http.status === 200){
+      cb(null,self.http.responseText);
 
+    }else{
+      cb('Error :' + self.http.status)
+    }
+  }
 
+  this.http.send();
+}
 
-
-
-
-
-
-
-
-
-
-
-
-// function easyHTTP(){
-//   this.http = new XMLHttpRequest();
-// }
-
-// // get function
-// easyHTTP.prototype.get = function(url,cb){
-//   this.http.open('GET',url, true);
-
-// // callback function occurs after the get request
-//   let self = this;
-
-//   this.http.onload = function(){
-//    if (self.http.status === 200){
-//      cb(null,self.http.responseText);
-//    }else{
-//     cb("Error : " + self.http.status)
-//     }
-//   }
-//    this.http.send();
-//   }
