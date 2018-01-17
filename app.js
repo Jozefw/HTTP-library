3d5f76534ae360d47493e53ca818c83c8557c755
Jozef <jozefw@sbcglobal.net>
2018-01-17 13:49:22 -0800

    add POST method

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 const http = new easyHTTP();
 
 
-http.get('https://jsonplaceholder.typicode.com/posts', function(err,posts){
+// http.get('https://jsonplaceholder.typicode.com/posts', function(err,posts){
 
-  if(err){
-    console.log(err);
-  }else{
-    console.log(posts);
-  }
+//   if(err){
+//     console.log(err);
+//   }else{
+//     console.log(posts);
+//   }
+// });
+
+
+// create data for post
+
+const data = {
+  "title": "New Post",
+  "body": "Just new rando post info"
+};
+
+http.post('https://jsonplaceholder.typicode.com/posts', data, function(err, post){
+    if(err){
+      console.log(err);
+    }else{
+      console.log(post);
+    }
+  
 });
--- easyhttp.js
+++ easyhttp.js
@@ -2,20 +2,48 @@ function easyHTTP(){
   this.http = new XMLHttpRequest();
 }
 
-// get request
+// ******* get request **********
 
 easyHTTP.prototype.get = function(url, cb) {
   this.http.open('GET',url,true);
- let self = this;
+  let self = this;
   this.http.onload = function(){
     if(self.http.status === 200){
       cb(null,self.http.responseText);
-
+      
     }else{
       cb('Error :' + self.http.status)
     }
   }
-
   this.http.send();
 }
+  
+  // ******* POST request **********
+  
+  easyHTTP.prototype.post = function(url, data,cb){
+    this.http.open('POST', url, true);
+    this.http.setRequestHeader('Content-type','application/json');
+    
+    let self = this;
+    this.http.onload = function(){
+      cb(null,self.http.responseText);
+    }
+    
+    this.http.send(JSON.stringify(data));
+  }
+  
+  
+  // ******* PUT request **********
 
+
+  easyHTTP.prototype.put = function(url, data,cb){
+    this.http.open('PUT', url, true);
+    this.http.setRequestHeader('Content-type','application/json');
+    
+    let self = this;
+    this.http.onload = function(){
+      cb(null,self.http.responseText);
+    }
+    
+    this.http.send(JSON.stringify(data));
+  }
